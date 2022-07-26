991f23c71755c8f677c927b9afbb00a054705470
Siddarth Dayala <97626996+siddarthdayala@users.noreply.github.com>
2022-07-27 00:09:03 +0530

    Update Even_digits.py

--- Searching and Sorting/Linear Search/Problems/Even_digits.py
+++ Searching and Sorting/Linear Search/Problems/Even_digits.py
@@ -1,1 +1,10 @@
+# Leetcode link - https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
+import math
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        for i in nums:
+            if int(math.log(i,10)+1)%2 == 0:    #log10 returns the number of digits in a number
+                count+=1
+        return count
