d5e36131c5cb964566896f7d449377d4ff36c9c7
Siddarth Dayala <97626996+siddarthdayala@users.noreply.github.com>
2022-08-15 12:42:00 +0530

    Create bubbleSort.py

---
+++ Searching and Sorting/Sorting/bubbleSort.py
@@ -0,0 +1,13 @@
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
+
+array = [9,7,6,5,4,3]
+print(bubbleSort(array))
+
+# Time complexity  -  O(n^2)
+#Space complexity  -  O(1)
